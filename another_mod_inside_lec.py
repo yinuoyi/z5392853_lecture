a7ed0d210361953b2c417e7e24c162eff7b1db1f
yinuoyi <129919162+yinuoyi@users.noreply.github.com>
2023-04-05 15:00:51 +1000

    Add files via upload

---
+++ another_mod_inside_lec.py
@@ -0,0 +1,10 @@
+""" another_mod_inside_lec.py
+ANother module inside 'lectures' package
+"""
+
+from lectures import mod_inside_lec
+
+mod_inside_lec
+
+
+
---
+++ mod_inside_lec.py
@@ -0,0 +1,6 @@
+""" mod_ inside_lec.py
+A module insed the 'lecturers' package
+"""
+
+print("You can see me")
+
